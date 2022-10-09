0bd2634a1e70da23824132bc374df85f6fd8cf98
Richard Jennings <richardjennings@gmail.com>
2022-10-09 13:05:05 +0100

    feat: add `extra_tags` option to set tags including additional registries (#134)

--- cmd/drone-docker-buildx/config.go
+++ cmd/drone-docker-buildx/config.go
@@ -159,6 +159,14 @@ func settingsFlags(settings *plugin.Settings, category string) []cli.Flag {
 			Category:    category,
 		},
 		&cli.StringSliceFlag{
+			Name:        "extra.tags",
+			EnvVars:     []string{"PLUGIN_EXTRA_TAGS"},
+			Usage:       "additional tags to use for the image including registry",
+			FilePath:    ".extratags",
+			Destination: &settings.Build.ExtraTags,
+			Category:    category,
+		},
+		&cli.StringSliceFlag{
 			Name:        "args",
 			EnvVars:     []string{"PLUGIN_BUILD_ARGS"},
 			Usage:       "custom build arguments for the build",
